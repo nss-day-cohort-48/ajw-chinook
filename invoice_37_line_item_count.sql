7b195514a29cd261690de1ca04e2b5848d1e5cef
Andrew Webb <webba@ihopu.org>
2021-08-23 15:25:36 -0500

    did exercises 8-10

--- invoice_37_line_item_count.sql
+++ invoice_37_line_item_count.sql
@@ -1,1 +1,5 @@
 -- Looking at the InvoiceLine table, provide a query that COUNTs the number of line items for Invoice ID 37.
+
+SELECT COUNT(invoiceid), invoiceid
+FROM invoiceline
+WHERE invoiceid = 37
--- total_sales_{year}.sql
+++ total_sales_{year}.sql
@@ -1,1 +1,5 @@
 -- What are the respective total sales for each of those years?
+
+SELECT SUM(total), invoicedate
+FROM invoice
+WHERE invoicedate < '2010-01-01' AND invoicedate > '2008-12-31' OR invoicedate < '2012-01-01' AND invoicedate > '2010-12-31'
